8d86c38a80e63b06d8bfc89594f144130bd4fc13
Don Nguyen <don78colorado@gmail.com>
2016-09-22 15:03:53 -0600

    use C++11

--- src/deque.cpp
+++ src/deque.cpp
@@ -15,12 +15,12 @@ void printIntDeque(deque<int> deq)
 int main()
 {
     //cout << "deque<int> deq = { 4, 6, 7 };\n";
-    //deque<int> deq = { 4, 6, 7 }; // Can't do this in C++98
-    deque<int> deq;
+    deque<int> deq = { 4, 6, 7 }; // Can't do this in C++98
+    /*deque<int> deq;
     deq.push_back(4);
     deq.push_back(6);
 deq.push_back(7);
-
+*/
     printIntDeque(deq);
 
     cout << "deq.push_front(2);\n";
